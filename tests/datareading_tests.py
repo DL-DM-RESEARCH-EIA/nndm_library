b3775145f1094820429f773eab230ef6bed693e0
andresg <Andres Gomez>
2022-05-10 11:41:37 -0500

    correct renaming of class

--- tests/datareading_tests.py
+++ tests/datareading_tests.py
@@ -1,13 +1,11 @@
 import unittest
-from unittest import result
-
-from nndm_library import ReadFile
+from nndm_library import ReadFileBase
 
 class ReadFileTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         name = "data/eta_decay_events_mk_0.014980679431428716_eps2_3.883209914996183e-10.lhe"
-        self.read_file = ReadFile(name)
+        self.read_file = ReadFileBase(name)
 
     def test_extract_params_from_name(self):
         type_particle, mk, eps2 = self.read_file.extract_params_from_name()

6bcb333c8c6539ce136a360cfe5a679bc8b17c24
andresg <Andres Gomez>
2022-05-27 20:50:45 -0500

    update gitignore and remove unncesary comments

--- nndm_library/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# from reading_utils import ReadFileBase
-# from reading_utils import ReadLhe
-# from reading_utils import FilesManipulator
-# from reading_utils import ReadRoot
-# from reading_utils import Constants
